8b869c5b07a27c204d1ca571dc8d2a5663fb08da
pvox <PPetrov@gksm.local>
2024-05-08 14:14:23 +0300

    020.loops.task4 commit

--- 020.loops.task4.py
+++ 020.loops.task4.py
@@ -7,10 +7,13 @@ for vlan_str in vlans_str_punctuation.split(","):
     if "-" in vlan_str:
         start_vlan = vlan_str.split("-")[0]
         end_vlan = vlan_str.split("-")[1]
-        vlans_list_int += tuple(vlan for vlan in range(int(start_vlan), int(end_vlan) + 1))
+        vlans_list_int += tuple(
+            vlan for vlan in range(int(start_vlan), int(end_vlan) + 1)
+        )
     else:
         vlans_list_int += (int(vlan_str),)
 
 for vlan in int("400"), int("800"):
-    print(f"vlan {vlan} {'входит' if vlan in vlans_list_int else 'не входит'} в список разрешенных на trunk'е vlan")
-
+    print(
+        f"vlan {vlan} {'входит' if vlan in vlans_list_int else 'не входит'} в список разрешенных на trunk'е vlan"
+    )
